258cffce73e1e12881c3a17ce41b4923d054a402
Sven Anders <sven@anders-hamburg.de>
2018-10-11 05:36:17 +0000

    fix icon set

--- test_marker.js
+++ test_marker.js
@@ -1,171 +1,35 @@
 
-
-
-
-
-
 function createBaseLayer(map) {
 	var layer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
 			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery Â© <a href="https://www.mapbox.com/">Mapbox</a>',
+			'Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
 		id: 'mapbox.streets'})
 	layer.addTo(map);	
 	return layer;
 }
 
-
-function Iconsetzen  (feature, latlng) {
-	
-		if ($('#OhneTempo30').is(':checked')) {
-			if (feature.properties.A === '1')  {icon = KitaGelb}
-			if (feature.properties.A ==='2') {icon = SchulIcongelb}
-			if (feature.properties.A === '3') {icon = AltenheimIcongelb}
-			if (feature.properties.A === '4') {icon = KrankenhausIcongelb}
-		}
-		
-		if ($('#MitTempo30').is(':checked')) {
-			 if (feature.properties.A === '1')  {icon = KitaGruen}
-			 if (feature.properties.A ==='2') {icon = SchulIcongruen}
-			 if (feature.properties.A === '3') {icon = AltenheimIcongruen}
-			 if (feature.properties.A === '4') {icon = KrankenhausIcongruen}
-		}
-		
-		
-		if ($('#Anforderungen').is(':checked')) {
-			 if (feature.properties.A === '1')  {icon = KitaIconrot}
-			 if (feature.properties.A ==='2') {icon = SchulIconrot}
-			 if (feature.properties.A === '3') {icon = AltenheimIconrot}
-			 if (feature.properties.A === '4') {icon = KrankenhausIconrot}
-		}
-		
-			return L.marker (latlng, { icon
-			});
-							}
-
-
-
-
 function Popup (feature, layer) {
-		layer.bindPopup (feature.properties.Name+ '<br>' +feature.properties.Straße +' '+feature.properties.Nummer);
-
-
-
+    layer.bindPopup (feature.properties.Name+ '<br>' +feature.properties.Straße +' '+feature.properties.Nummer);
 }
 
-	var KitaIconrot = L.icon({
-			iconUrl: 'iconFarben/kidsrot.png', 
-			iconSize: [18, 18],
-			iconAnchor: [9, 9],
-			popupAnchor: [ 0, 0],
-	});	
-	
-	var KitaGelb = L.icon({
-			iconUrl: 'iconFarben/kidsgelb.png', 
-			iconSize: [18, 18],
-			iconAnchor: [9, 9],
-			popupAnchor: [ 0, 0],
-		
-		
-	});	
-	
-	var KitaGruen = L.icon({
-			iconUrl: 'iconFarben/kidsgruen.png', 
-			iconSize: [18, 18],
-			iconAnchor: [9, 9],
-			popupAnchor: [ 0, 0],
-			
-			
-	});	
-	
-	var SchulIconrot = L.icon({
-			iconUrl: 'iconFarben/classroomrot.png', 
-			iconSize: [18, 18],
-			iconAnchor:[9, 9],
-			popupAnchor: [ 0, 0],
-			
-			
-	});	
-	
-	var SchulIcongruen = L.icon({
-			iconUrl: 'iconFarben/classroomgruen.png', 
-			iconSize: [18, 18],
-			iconAnchor:[9, 9],
-			popupAnchor: [ 0, 0],
-			
-			
-	});	
-	
-	var SchulIcongelb = L.icon({
-			iconUrl: 'iconFarben/classroomgelb.png', 
-			iconSize: [18, 18],
-			iconAnchor:[9, 9],
-			popupAnchor: [ 0, 0],
-			
-			
-	});	
-	
-				
-	var AltenheimIconrot = L.icon({
-			iconUrl: 'iconFarben/helprot.png', 
-			iconSize: [18, 18],
-			iconAnchor: [9, 9],
-			popupAnchor: [ 0, 0],
-			
-			
-	});	
-	
-	var AltenheimIcongruen = L.icon({
-			iconUrl: 'iconFarben/helpgruen.png', 
-			iconSize: [18, 18],
-			iconAnchor: [9, 9],
-			popupAnchor: [ 0, 0],
-		
-		
-			
-	});	
-	
-	var AltenheimIcongelb = L.icon({
-			iconUrl: 'iconFarben/helpgelb.png', 
-			iconSize: [18, 18],
-			iconAnchor: [9, 9],
-			popupAnchor: [ 0, 0],
-			
-			
-	});	
-					
-	var KrankenhausIconrot = L.icon({
-			iconUrl: 'iconFarben/hospitalrot.png', 
-			iconSize: [18, 18],
-			iconAnchor: [9, 9],
-			popupAnchor: [ 0, 0],
-		
-		
-	});				
-	
-	var KrankenhausIcongruen = L.icon({
-			iconUrl: 'iconFarben/hospitalgruen.png', 
-			iconSize: [18, 18],
-			iconAnchor: [9, 9],
-			popupAnchor: [ 0, 0],
-			
-			
-	});				
-	
-	var KrankenhausIcongelb = L.icon({
-			iconUrl: 'iconFarben/hospitalgelb.png', 
-			iconSize: [18, 18],
-			iconAnchor: [9, 9],
-			popupAnchor: [ 0, 0],
-			
-			
-	});				
-	
-	
+function createLayer (map, checkboxID, URL, iconSet) {
+    function iconSetzen(feature, latlng) {
+        var icon;
+        if (feature.properties.A === '1')  {
+            icon = iconSet.Kita;
+        } else if (feature.properties.A ==='2') {
+            icon = iconSet.Schule;
+        } else if (feature.properties.A === '3') {
+            icon = iconSet.Altenheim;
+        } else {
+	    // feature.properties.A === '4'
+            icon = iconSet.Krankenhaus;
+        }
+        return L.marker(latlng, { 'icon': icon });
 
-function createLayer (map, checkboxID, URL) {
-	
+    }
 	var layer;
 	var $checkbox = $('#'+checkboxID);
 	$checkbox.click (function (){
@@ -177,7 +41,7 @@ function createLayer (map, checkboxID, URL) {
 					success: function (data) {
 						layer = L.geoJSON(data, {
 							'onEachFeature': Popup,
-							'pointToLayer': Iconsetzen 
+							'pointToLayer': iconSetzen 
 							
 								
 							});
@@ -196,23 +60,42 @@ function createLayer (map, checkboxID, URL) {
 }
 
 			
-						
+function createIconSet(farbe) {
+    var KitaIcon = L.icon({
+	iconUrl: 'iconFarben/kids'+farbe+'.png', 
+	iconSize: [18, 18],
+	iconAnchor: [9, 9],
+	popupAnchor: [ 0, 0],
+    });
+    var SchulIcon = L.icon({
+	iconUrl: 'iconFarben/classroom'+farbe+'.png', 
+	iconSize: [18, 18],
+	iconAnchor:[9, 9],
+	popupAnchor: [ 0, 0],
+    });	
+    var AltenheimIcon = L.icon({
+	iconUrl: 'iconFarben/help'+farbe+'.png', 
+	iconSize: [18, 18],
+	iconAnchor: [9, 9],
+	popupAnchor: [ 0, 0],
+    });	
+    var iconSet = {
+        'Kita':KitaIcon,
+        'Schule':SchulIcon,
+        'Altenheim':AltenheimIcon
+    };
+    return iconSet;
+}
 	
 	
 	
 jQuery(document).ready(function(){
-	var mymap = L.map('mapid').setView([53.55, 10.09], 10);
-	var baseLayer = createBaseLayer(mymap);
-	
-
-	
-	
-createLayer(mymap, 'MitTempo30', 'data/MIT_TEMPO30.json');
-	
-createLayer(mymap, 'OhneTempo30', 'data/KEIN_TEMPO30.json');
-	
-createLayer(mymap, 'Anforderungen', 'data/trotzdem_nicht_Tempo30.json');	
-	
+    var mymap = L.map('mapid').setView([53.55, 10.09], 10);
+    var baseLayer = createBaseLayer(mymap);
+    
+    createLayer(mymap, 'MitTempo30', 'data/MIT_TEMPO30.json', createIconSet('gruen'));
+    createLayer(mymap, 'OhneTempo30', 'data/KEIN_TEMPO30.json', createIconSet('gelb'));
+    createLayer(mymap, 'Anforderungen', 'data/trotzdem_nicht_Tempo30.json', createIconSet('rot'));		
 });
 	
 	
